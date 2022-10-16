c392415fd38a608185daf5c575c19286c7c9c9a6
Reginal Sengkey <sengkey@gmail.com>
2022-10-16 20:15:12 +1100

    Added rel="noreferrer" to href

--- src/components/molecules/Header/Header.js
+++ src/components/molecules/Header/Header.js
@@ -3,7 +3,7 @@ import style from "./Header.module.scss";
 
 const Header = () => (
   <header>
-    <a href="https://www.qantas.com/au/en.html" target="_blank"><img src={logo} className={style.logo} alt="Qantas" /></a>
+    <a href="https://www.qantas.com/au/en.html" target="_blank" rel="noreferrer" ><img src={logo} className={style.logo} alt="Qantas" /></a>
   </header>
 );
 
